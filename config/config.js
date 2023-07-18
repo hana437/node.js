32e02b5ff2321c80e0ec97a2f4f6a5abed1ae1a8
hana437 <hanahabtemicheal@gmail.com>
2023-07-18 23:39:59 +0300

    config created

---
+++ config/config.js
@@ -0,0 +1,16 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+module.exports = {
+  db: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    name: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+  },
+  jwt: {
+    secret: process.env.JWT_SECRET,
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  },
+};
